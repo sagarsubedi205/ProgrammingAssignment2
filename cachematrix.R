05b3812941729930042922d2526ea96388bc8c54
sagarsubedi205 <sagarsubedi205@users.noreply.github.com>
2020-09-29 11:43:47 +0545

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-	s <- NULL
-	set <- function(y) {
-		x <<- y
-		s <<- NULL	
-}
-	get <- function() x
-  	setsolve <- function(solve) s <<- solve
- 	 getsolve <- function() s
- 	 list(set = set, get = get,
-       	setsolve = setsolve,
-      	getsolve = getsolve)
-}
 
 ## Write a short comment describing this function
 
@@ -24,6 +6,36 @@ makeCacheMatrix <- function(x = matrix()) {
  		 if(!is.null(s)) {
    			 message("getting inversed matrix")
    			 return(s)
+##Please include your own comment to explain your code
+
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+  x <<- y
+  j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
+}
+
+##Please include your own comment to explain your code
+
+cacheSolve <- function(x, ...) {
+## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+  message("getting cached data")
+  return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
         ## Return a matrix that is the inverse of 'x'
 }
 	data <- x$get()
